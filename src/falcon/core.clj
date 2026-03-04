137589632e401ce16bcdc9d4bb48b898d66af99a
Caleb <beerscb@gmail.com>
2026-03-04 11:27:17 -0500

    Initial commit

---
+++ src/falcon/core.clj
@@ -0,0 +1,8 @@
+;; Where would a `require` statement go in this Clojure code?
+(ns falcon.core
+  (:require '[etaoin.api :as e]
+            '[etaoin.keys :as k]
+            '[etaoin.string :as str]))
+
+(defn -main [& args]
+  ())
